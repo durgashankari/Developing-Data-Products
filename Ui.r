41c2a2df9b93f1f0742c37596958d6fb78748453
durgashankari <cntme2sds@gmail.com>
2017-01-19 21:19:46 -0600

    Update Ui.r

--- Ui.r
+++ Ui.r
@@ -1,33 +1,33 @@
-#install.packages("shiny")
 library(shiny)
 
-shinyUI( pageWithSidebar(
-        # Application title
-        headerPanel("Guess The Number!"),
-        
-        sidebarPanel(
-                #numericInput('guess', 'Number', 1, min = 1, max = 100, step = 1),
-                #submitButton('Submit')
-                textInput('guess', 'Number', value = ""),
-                h5('Please press \'Go!\' only on your first attempt'),
-                actionButton("goButton", "Go!")
-        ), 
-        mainPanel(
-                h2('Guess an integer between 1 and 100'),
-                h5('The objective of this game is to guess an integer between
-                   1 and 100 (inclusive) and try to match it with the computer\'s
-                   selection'),
-                h3('Results of guess'),
-                h4('You entered'),
-                verbatimTextOutput("inputValue"),
-                h4('Which is...'),
-                verbatimTextOutput("outputValue"),
-                h5('Please note that the application will continue running even
-                   after you have guessed the number correctly. This is due to the
-                   app developer\'s lack of skills/experience that has left this app
-                   flawed. Apologies.'),
-                h5('For a more comprehensive, better and visually pleasing version of
-                   this game (from an awesome developer))
-        )
-)
-)
+langs <- c("italian")
+
+shinyUI(fluidPage(
+  headerPanel("Headline News - Categorization"),
+  sidebarPanel(
+    wellPanel(
+      helpText(   a("Click Here to read the documentation",
+                    href="https://github.com/leandrowar/DevelopingDataProducts/blob/master/README.md")
+      )
+    ),
+    
+    textInput("headline", "Headline - Please, insert a headline in Italian", 
+              value = "Moscovici: Piu flessibilita allItalia ma fate gli sforzi per le riforme"),
+    
+    selectInput("lang", "Select the language:", choices = langs,
+                selected = "italian"),
+    
+    submitButton("Go")
+  ),
+  
+  mainPanel(
+    
+    h3("Headline Categorization"),
+    h4("The entered headline is:"),
+    verbatimTextOutput("headline"),
+    h4("And your content is: "),
+    verbatimTextOutput("categorization")
+
+  )
+  
+))
